4bb4c4bed7b0a2eaa737bec3aa990cccbcfd51ad
Jonas Minnberg <sasq64@gmail.com>
2019-03-13 14:33:53 +0100

    build.rs now static links all found libraries in cmake output

--- build.rs
+++ build.rs
@@ -1,11 +1,39 @@
 extern crate cmake;
 use cmake::Config;
+use std::io;
+use std::fs::{self, DirEntry};
+use std::path::{Path, PathBuf};
+
+fn visit_dirs(dir: &Path, target : &mut Vec<PathBuf>) {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir).unwrap() {
+            let path = entry.unwrap().path();
+            if path.is_dir() {
+                visit_dirs(&path, target);
+            } else {
+                println!("{}", path.to_str().unwrap());
+                let name : String = path.file_name().unwrap().to_str().unwrap().to_string();
+                if name.ends_with(".a") {
+                    println!("cargo:rustc-link-search=native={}", dir.to_str().unwrap());
+                    let len = name.len();
+                    println!("cargo:rustc-link-lib=static={}", name[3..len-2].to_string());
+                } 
+
+
+                target.push(path);
+            }
+        }
+    }
+}
 
 fn main() {
     let dst = Config::new("musicplayer").build();
 
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=dylib=musix");
-    //println!("cargo:rustc-link-lib=dylib=stdc++");
+    let mut paths = Vec::<PathBuf>::new();
+    visit_dirs(&dst, &mut paths);
+
+    //println!("cargo:rustc-link-search=native={}", dst.display());
+    //println!("cargo:rustc-link-lib=dylib=musix");
+    println!("cargo:rustc-link-lib=dylib=c++");
     //    println!("cargo:rustc-link-lib=dylib=asound");
 }
