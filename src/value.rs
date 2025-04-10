4b8a04bce16b2218145c406d178d4f24732a0712
Jonas Minnberg <sasq64@gmail.com>
2025-04-10 22:57:12 +0200

    Missing file

---
+++ src/value.rs
@@ -0,0 +1,53 @@
+use core::fmt;
+use std::fmt::Display;
+
+use musix::MusicError;
+
+pub(crate) enum Value {
+    Text(String),
+    Number(i32),
+    Data(Vec<u8>),
+    Error(MusicError),
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Value::Text(s) => f.write_str(s.as_str())?,
+            Value::Number(n) => write!(f, "{n:02}")?,
+            Value::Error(e) => write!(f, "{e}")?,
+            Value::Data(_) => write!(f, "Data")?,
+        }
+        Ok(())
+    }
+}
+
+impl From<i32> for Value {
+    fn from(item: i32) -> Self {
+        Value::Number(item)
+    }
+}
+
+impl From<String> for Value {
+    fn from(item: String) -> Self {
+        Value::Text(item)
+    }
+}
+
+impl From<&str> for Value {
+    fn from(item: &str) -> Self {
+        Value::Text(item.to_owned())
+    }
+}
+
+impl From<Vec<u8>> for Value {
+    fn from(item: Vec<u8>) -> Self {
+        Value::Data(item)
+    }
+}
+
+impl From<MusicError> for Value {
+    fn from(item: MusicError) -> Self {
+        Value::Error(item)
+    }
+}
